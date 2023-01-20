b7c7eab55dbe5dd7d822dae81c456edacf41282a
paul_nasdalack <paul@indieburg.com>
2023-01-20 13:34:52 +0100

    Image Skipping

--- Runtime/Client/SkipReason.cs
+++ Runtime/Client/SkipReason.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,4 +13,25 @@ namespace ImageAnnotation.Client
 		TooManyCraters, 
 		Other
     }
+
+	public static class SkipReasonExtensions
+	{
+		public static string ToServerString(this SkipReason reason)
+		{
+			switch(reason)
+			{
+				case SkipReason.TooDark:
+					return "too_dark";
+				case SkipReason.TooBright:
+					return "too_bright";
+				case SkipReason.ImageCorrupted:
+					return "image_corrupted";
+				case SkipReason.TooManyCraters:
+					return "too_many_craters";
+				case SkipReason.Other:
+					return "other";
+			}
+			throw new NotImplementedException($"Missing Server String for reason {reason.ToString()}");
+		}
+	}
 }
